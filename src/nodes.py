6a43f10b3e4e70e5e2476696dcd64a54bb862bf0
Onur Karaman <karaman1@illinois.edu>
2011-11-17 03:05:43 -0600

    Added if and while nodes; Added untested error handling nodes

--- src/nodes.py
+++ src/nodes.py
@@ -99,6 +99,52 @@ class BinOpNode(Node):
         else:
             return '%s %s %s' % (leftOperand, operator, rightOperand)
 
+class WhileNode(Node):
+    def compile(self):
+        condition = Node(self.node.test).compile()
+        whileBlock = '\r\n'.join(Node(stmt).compile() for stmt in self.node.body)
+        elseBlock = '\r\n'.join(Node(stmt).compile() for stmt in self.node.orelse)
+        return 'while(%s){%s}%s' % (condition, whileBlock, elseBlock)
+
+class IfNode(Node):
+    def compile(self):
+        hasElse = bool(self.node.orelse)
+        condition = Node(self.node.test).compile()
+        ifBlock = '\r\n'.join(Node(stmt).compile() for stmt in self.node.body)
+        if hasElse:
+            elseBlock = '\r\n'.join(Node(stmt).compile() for stmt in self.node.orelse)
+            return 'if(%s){%s}else{%s}' % (condition, ifBlock, elseBlock)
+        else:
+            return 'if(%s){%s}' % (condition, ifBlock)
+
+#untested
+class RaiseNode(Node):
+    def compile(self):
+        exceptionType = Node(self.node.type).compile()
+        return 'throw %s + ":" + %s;' % ( exceptionType, Node(self.node.inst).compile() )
+
+#untested
+class TryExceptNode(Node):
+    def compile(self):
+        tryBody = '\r\n'.join(Node(stmt).compile() for stmt in self.node.body)
+        handlers = Node(self.node.handlers.body).compile()
+        catchBody = '\r\n'.join(Node(stmt).compile() for stmt in self.node.orelse)
+        return 'try{%s}catch(e){var caughtException = true;%s}if(!caughtException){%s}' % (tryBody, handlers, catchBody)
+
+#untested
+class TryFinallyNode(Node):
+    def compile(self):
+        tryBody = '\r\n'.join(Node(stmt).compile() for stmt in self.node.body)
+        finallyBody = '\r\n'.join(Node(stmt).compile() for stmt in self.node.finalbody)
+        return 'try{%s}finally{%s}' % (tryBody, finallyBody)
+
+#untested
+class FunctionDefNode(Node):
+    def compile(self):
+        functionName = self.node.name
+        body = '\r\n'.join(Node(stmt).compile() for stmt in self.node.body)
+        return 'this.%s = function(args){%s}' % (functionName, body)
+
 class ReturnNode(Node):
     def compile(self):
         return 'return %s;' % ('' if self.node.value is None else Node(self.node.value).compile())
