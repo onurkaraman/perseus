3201dedc312edb0df4bd83957799ec82da3f969c
Jiawei Li <jiawei.h.li@gmail.com>
2011-11-17 17:05:38 -0600

    refactoring names, redid logic for factory creation

--- src/newNodes.py
+++ src/newNodes.py
@@ -6,42 +6,44 @@ def getClassName(object):
     return type(object).__name__
 
 # rootNode = ModuleNode(ast.parse(...), None)
-class Node:
+class Base:
     def __init__(self, astNode, parent):
         self = astNode
         self.parent = parent
         
         # Auto-build nodes out of children fields.
-        for child in self._fields:
-            self.setattr(child, self.buildChildNode(child))
-            
-    def buildChildNode(astNode):
-        nodeType = getClassName(astNode)
-        nodeClass = globals()[nodeType]
-        return nodeClass(astNode, self)    
+        for fieldName in self._fields:
+            child = self.getattr(field)
+            child = self.resolve(child)
+    
+    def resolve(element):
+        '''
+        Returns an element if it is primitive or casts an ast.AST object into
+        its abstraction.
+        '''
+        # Handling primitive children
+        if type(element) in __builtins__:
+            return element 
+        else:
+            className = getClassName(element)
+            nodeClass = globals()[className]
+            return nodeClass(element, self)    
     def compile(self):
         pass
     def __str__(self):
         return self.compile()
 
-class FloorDivNode(Node):
+class FloorDiv(Base):
     def compile(self):
         return 'Math.floor(%s, %s)' % (self.parent.left, self.parent.right)
 
-class BinOpNode(Node):
+class BinOp(Base):
     def compile(self):
-#        leftOperand = self.left
-#        operator = self.op
-#        rightOperand = self.right
-#        if(getClassName(operator) in ['Pow', 'FloorDiv']):
-#            return '%s(%s, %s)' % (operator, leftOperand, rightOperand)
-#        else:
-#            return '%s %s %s' % (leftOperand, operator, rightOperand)
         return self.op.compile()
 
-class Num(Node):
+class Num(Base):
     def compile(self):
-        return '%s' % self.node.n
+        return '%s' % self.n
 
 class Block:
     def __init__(self, astNodeList, parent):
@@ -57,5 +59,3 @@ class Block:
     # plus 1
     def calcIndent(self):
         pass
-    
-    
