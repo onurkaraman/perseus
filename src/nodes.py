3fab555a0bd3e2bad633cade5eeece3de4347098
Jiawei Li <jiawei.h.li@gmail.com>
2011-11-17 04:41:52 -0600

    fixing IfExpNode

--- src/nodes.py
+++ src/nodes.py
@@ -133,8 +133,8 @@ class UnaryOpNode(Node):
 class IfExpNode(Node):
     def compile(self):
         condition = Node(self.node.test).compile()
-        ifBody = ChunkNode(self.node.body).compile()
-        elseBody = ChunkNode(self.node.body).compile() # unsure if this should be ChunkNode or Node
+        ifBody = Node(self.node.body).compile()
+        elseBody = Node(self.node.orelse).compile()
         return '%s ? %s : %s' % (condition, ifBody, elseBody)
 
 class PrintNode(Node):
