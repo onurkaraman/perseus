ec1a4a1e5f7886e2ffe47423d36f6a5456020b55
Onur Karaman <karaman1@illinois.edu>
2012-04-18 01:34:41 -0500

    Updated tester to nicely output source code on failure and added formatText helper function

--- test/tester.py
+++ test/tester.py
@@ -7,36 +7,41 @@ import subprocess
 
 TEST_PATH = './tests/'
 
+def formatText(text, indentLevel):
+    delimiter = '\n' + '\t' * indentLevel
+    return delimiter.join(text.splitlines())
+
 def outputResults(testFileName, pythonOutput, jsInput, jsOutput):
-	if pythonOutput == jsOutput:
-		print 'PASS: %s' % testFileName
-	else:
-		formattedPyOutput = '\n\t\t'.join(pythonOutput.splitlines())
-		formattedJsOutput = '\n\t\t'.join(jsOutput.splitlines())
-		print 'FAIL: %s\n\tExpected:\n\t\t%s\n\tActual:\n\t\t%s' % (testFileName, formattedPyOutput, formattedJsOutput)
-        print 'SOURCE: \n%s\n' % jsInput
+    if pythonOutput == jsOutput:
+        print 'PASS: %s' % testFileName
+    else:
+        formattedPyOutput = formatText(pythonOutput, 2)
+        formattedJsOutput = formatText(jsOutput, 2)
+        formattedJsInput = formatText(jsInput, 2)
+        print 'FAIL: %s\n\tExpected:\n\t\t%s\n\tActual:\n\t\t%s\n\tSource:%s\n\t\t' % (testFileName, formattedPyOutput, formattedJsOutput, formattedJsInput)
 
 def runTest(pyTestFileName):
-	pyTestFilePath = TEST_PATH + pyTestFileName
-	pythonOutput = subprocess.check_output(['python', pyTestFilePath])
-	perseus = open('../perseus.js','r').read()
-	jsTestFileName = 'tmpTest.js'
-	try:
-		jsTest = subprocess.check_output(['python', 'compiler.py', pyTestFilePath])
-
-		jsTestFile = open(jsTestFileName, 'w+')
-		jsTestFile.write(perseus + jsTest)
-		jsTestFile.close()
-
-		jsOutput = subprocess.check_output(['node', jsTestFileName])
-		outputResults(pyTestFileName, pythonOutput, jsTest, jsOutput)
-	except Exception as exception:
-		outputResults(pyTestFileName, pythonOutput, jsTest, str(exception))
-	finally:
-		if os.path.exists(jsTestFileName):
-			os.remove(jsTestFileName)
+    pyTestFilePath = TEST_PATH + pyTestFileName
+    pythonOutput = subprocess.check_output(['python', pyTestFilePath])
+    perseus = open('../perseus.js','r').read()
+    jsTestFileName = 'tmpTest.js'
+    try:
+        jsTest = subprocess.check_output(['python', 'compiler.py', pyTestFilePath])
+    except Exception as exception:
+        outputResults(pyTestFileName, pythonOutput, '[Compilation Error]', str(exception))
+        return
+    with open(jsTestFileName, 'w+') as jsTestFile:
+        jsTestFile.write(perseus + jsTest)
+    try:
+        jsOutput = subprocess.check_output(['node', jsTestFileName])
+        outputResults(pyTestFileName, pythonOutput, jsTest, jsOutput)
+    except Exception as exception:
+        outputResults(pyTestFileName, pythonOutput, jsTest, str(exception))
+    finally:
+        if os.path.exists(jsTestFileName):
+            os.remove(jsTestFileName)
 
 if __name__ == '__main__':
-	pyTestFileNames = os.listdir(TEST_PATH)
-	for pyTestFileName in pyTestFileNames:
-		runTest(pyTestFileName)
+    pyTestFileNames = os.listdir(TEST_PATH)
+    for pyTestFileName in pyTestFileNames:
+        runTest(pyTestFileName)
